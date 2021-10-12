adffae7db02710aa52b9755bf4251f52515abd70
Frode Hansen <frodekh@gala.lan>
2021-10-12 22:56:44 +0200

    oppdaterte 2A del1 og del2

---
+++ add_pagenumbers_advanced.py
@@ -0,0 +1,45 @@
+
+
+filename = '2A/interaktive_forelesningsnotater_2A_del2_av_2.tex'
+
+with open(filename) as file:
+    lines = file.readlines()
+    
+pg = 0
+nytemas = 0
+pgnytemas = []
+for line in lines:
+    if ('frame' in line) and (not ('newcommand' in line)) and (not ('begin' in line)) and (not ('end' in line)): pg += 1
+    if ('nytemaside' in line):
+        nytemas += 1
+        pgnytemas.append(pg)
+totpg = pg
+pgnytemas.append(totpg)
+
+pg = 1
+nytemas = 0
+lineind = 0
+inframe = False
+for line in lines:
+    line = line.rstrip()
+    if line == '}': lastparan = lineind
+    if ('frame' in line) and (not ('newcommand' in line)) and (not ('begin' in line)) and (not ('end' in line)) and (not ('mp4' in line)) and not inframe:
+        inframe = True
+    elif ('frame' in line) and (not ('newcommand' in line)) and (not ('end' in line)) and (not ('mp4' in line)) and inframe:
+        ln = lines[lastparan].rstrip()
+        lines[lastparan] = ln + '{SIDE '+str(pg)+'/'+str(pgnytemas[nytemas]-2)+'/'+str(totpg-2)+'}' + '\r'
+        pg += 1
+        if  ('frame' in line) and  ('begin' in line): inframe = False
+    if ('nytemaside' in line): nytemas += 1
+    lines[lineind] = line + '\r'
+    lineind += 1
+
+
+
+
+    
+with open(filename, 'w') as f:
+    for item in lines:
+        f.write("%s\n" % item)
+
+        
---
+++ add_pagenumbers_advanced.py~
@@ -0,0 +1,40 @@
+
+
+filename = '3B/interaktive_forelesningsnotater_3B.tex'
+
+with open(filename) as file:
+    lines = file.readlines()
+    
+pg = 0
+nytemas = 0
+pgnytemas = []
+for line in lines:
+    if ('lastpagebutton' in line) and (not ('newcommand' in line)) and (not ('#' in line)): pg += 1
+    if ('nytemaside' in line):
+        nytemas += 1
+        pgnytemas.append(pg)
+totpg = pg
+pgnytemas.append(totpg)
+
+pg = 0
+nytemas = 0
+lineind = 0
+for line in lines:
+    line = line.rstrip()
+    if ('lastpagebutton' in line) and (not ('newcommand' in line)) and (not ('#' in line)):
+        pg += 1
+        line += '{\\bf SIDE '+str(pg)+'/'+str(pgnytemas[nytemas])+'/'+str(totpg)+'}'+r"\\"
+    if ('nytemaside' in line): nytemas += 1
+    lines[lineind] = line + '\r'
+    lineind += 1
+
+
+
+
+
+    
+with open(filename, 'w') as f:
+    for item in lines:
+        f.write("%s\n" % item)
+
+        
