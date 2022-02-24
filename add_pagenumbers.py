2e73e5eec736c1b519c1e5f9bf0000773d252e54
Frode Hansen <frodekh@gala.lan>
2022-02-24 18:15:33 +0100

    oppdaterte pagebuttonoling, store/små bokstaver og sidetall for alle notater

--- add_pagenumbers.py
+++ add_pagenumbers.py
@@ -1,28 +1,40 @@
 
+import regex as re
 
-filename = '3C/interaktive_forelesningsnotater_3C.tex'
+filename = '1G/interaktive_forelesningsnotater_1G.tex'
 
 with open(filename) as file:
     lines = file.readlines()
-    
+
+multistr = re.compile('.+_.}') ## check if label is of the form '*_?}' (i.e. (\label{ddd_a})
+multistr2 = re.compile('.+_a}') ##exclude the first page with _a
 pg = 0
 nytemas = 0
 pgnytemas = []
+lastlabel = ''
+lineind = 0
+
+print
+
 for line in lines:
-    if ('lastpagebutton' in line) and (not ('newcommand' in line)) and (not ('#' in line)): pg += 1
+    if ('\label' in line): lastlabel = lineind
+    if ('lastpagebutton' in line) and (not ('newcommand' in line)) and (not ('#' in line)) and not (bool(re.match(multistr,lines[lastlabel])) and not bool(re.match(multistr2,lines[lastlabel]))): pg += 1 #; print(pg,line,lines[lastlabel])
     if ('nytemaside' in line):
         nytemas += 1
         pgnytemas.append(pg)
+    lineind += 1
 totpg = pg
 pgnytemas.append(totpg)
 
 pg = 0
 nytemas = 0
 lineind = 0
+lastlabel = ''
 for line in lines:
+    if ('\label' in line): lastlabel = lineind
     line = line.rstrip()
     if ('lastpagebutton' in line) and (not ('newcommand' in line)) and (not ('#' in line)):
-        pg += 1
+        if not (bool(re.match(multistr,lines[lastlabel])) and not bool(re.match(multistr2,lines[lastlabel]))): pg += 1 #; print(pg,line,lines[lastlabel])
         line += '{\\bf SIDE '+str(pg)+'/'+str(pgnytemas[nytemas])+'/'+str(totpg)+'}'+r"\\"
     if ('nytemaside' in line): nytemas += 1
     lines[lineind] = line + '\r'
--- add_pagenumbers_advanced.py
+++ add_pagenumbers_advanced.py
@@ -1,6 +1,9 @@
 
+####NOTE implementation for not changing page number when _b, _c, etc. not finished, missing check in last part
 
-filename = '2D/interaktive_forelesningsnotater_2D.tex'
+import regex as re
+
+filename = '1A/interaktive_forelesningsnotater_1A_del1_av_2.tex'
 
 with open(filename) as file:
     lines = file.readlines()
@@ -8,11 +11,15 @@ with open(filename) as file:
 pg = 0
 nytemas = 0
 pgnytemas = []
+multistr = re.compile('.+_.}.+') ## check if label is of the form '*_?)*' (i.e. (\label{ddd_a})
+lineind = 0
 for line in lines:
-    if ('frame' in line) and (not ('newcommand' in line)) and (not ('begin' in line)) and (not ('end' in line)): pg += 1
+#    if ('frame' in line) and (not ('newcommand' in line)) and (not ('number' in line)) and (not ('begin' in line)) and (not ('end' in line)) and not bool(re.match(multistr,lines[lineind + 1])): pg += 1
+    if ('frame' in line) and (not ('newcommand' in line)) and (not ('number' in line)) and (not ('begin' in line)) and (not ('end' in line)): pg += 1
     if ('nytemaside' in line):
         nytemas += 1
         pgnytemas.append(pg)
+    lineind += 1
 totpg = pg
 pgnytemas.append(totpg)
 
@@ -23,8 +30,9 @@ inframe = False
 for line in lines:
     line = line.rstrip()
     if line == '}': lastparan = lineind
-    if ('frame' in line) and (not ('newcommand' in line)) and (not ('begin' in line)) and (not ('end' in line)) and (not ('.mp4' in line)) and not inframe:
+    if ('frame' in line) and (not ('newcommand' in line)) and (not ('number' in line)) and (not ('begin' in line)) and (not ('end' in line)) and (not ('.mp4' in line)) and not inframe:
         inframe = True
+        #print(line)
     elif ('frame' in line) and (not ('newcommand' in line)) and (not ('end' in line)) and (not ('.mp4' in line)) and inframe:
         ln = lines[lastparan].rstrip()
         lines[lastparan] = ln + '{SIDE '+str(pg)+'/'+str(pgnytemas[nytemas]-0)+'/'+str(totpg-0)+'}' + '\r'
